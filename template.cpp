f5d2c3070d89e31ef168bf4a7f70a4a252bc4bd8
Oliver Xu <olxu@hotmail.com>
2025-07-29 23:54:34 +1000

    template.cpp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#define D(x...) fprintf(stderr,x)
+#else
+#define D(x...)
+#endif
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+
+const ll MOD = (ll)(1e9)+7ll;
+const ll INF = (1ll << 60);
+
+int main() {
+  ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	return 0;
+}
