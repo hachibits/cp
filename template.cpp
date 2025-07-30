7a38f8dbca07f00addbaaaf6f38009356964fa68
Oliver Xu <olxu@hotmail.com>
2025-07-31 02:02:07 +1000

    debug

--- template.cpp
+++ template.cpp
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef DEBUG
-#define D(x...) fprintf(stderr,x)
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << H;
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 #else
-#define D(x...)
+#define debug(...) 42
 #endif
 
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
+
 typedef long long ll;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
-typedef vector<int> vi;
 
 const ll MOD = (ll)(1e9)+7ll;
 const ll INF = (1ll << 60);
 
 int main() {
   ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	return 0;
+  cin.tie(0);
+  return 0;
 }
