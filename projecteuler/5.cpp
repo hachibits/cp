24ca7e796a92731abe9c80e4b9975db9bc8d585c
Oliver Xu <olxu@hotmail.com>
2025-08-10 17:22:03 +1000

    5

---
+++ projecteuler/5.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << H;
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+const ll MOD = (ll)(1e9)+7ll;
+const ll INF = (1ll << 60);
+
+long long gcd(long long a, long long b) {
+  if (b == 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+}
+
+long long lcm(long long a, long long b) {
+  return a * (b / gcd(a,b));
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  long long ans = 1;
+  for (int i = 2; i <= 20; i++) {
+    ans = lcm(ans, i);
+  }
+  cout << ans << '\n';
+  return 0;
+}
