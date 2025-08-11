d954316317578646b46cae207023995945e91c5b
Oliver Xu <olxu@hotmail.com>
2025-08-12 02:52:42 +1000

    8

---
+++ projecteuler/8.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << H;
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+const ll MOD = (ll)(1e9)+7ll;
+const ll INF = (1ll << 60);
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  string s, t;
+  for (int i = 0; i < n; i++) {
+    cin >> t;
+    s += t;
+  }
+  unsigned long long mx = 0;
+  for (size_t i = 0; i < (size_t) s.size() - 14; i++) {
+    unsigned long long p = 1;
+    for (size_t j = i; j < i + 13; j++) {
+      p *= (unsigned) s[j] - '0';
+    }
+    mx = max(mx, p);
+  }
+  cout << mx << '\n';
+  return 0;
+}
